4bc671adc4f55409a0a533821a6c6d4d701c6e82
Ursys <jeb.mccluskey@gmail.com>
2014-06-11 09:56:45 -0400

    Change pythonTest.py to Cube Root function

--- pythonTest.py
+++ pythonTest.py
@@ -1,7 +1,14 @@
-userInput = int(raw_input ('Please enter an integer\n'))
-if userInput % 2 == 0:
-    print(str(userInput) + ' is an EVEN number')
+sumNumber = 5+2
+# Find the cube root of a perfect cube
+x = int(raw_input('Enter an integer: '))
+ans = 0
+while ans*ans*ans < abs(x):
+    ans = ans + 1
+    print 'current guess = ', ans
+if ans*ans*ans != abs(x):
+    print x, 'is not a perfect cube'
 else:
-    print(str(userInput) + ' is an ODD number')
-    
-    
+    if x < 0:
+        ans = -ans
+    print 'Cube root of ' + str(x) + ' is ' + str(ans)
+        
